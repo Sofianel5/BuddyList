94f43454208b681b703b25a40188921c3f69f0b6
Ubuntu <ubuntu@ip-172-31-78-140.ec2.internal>
2021-03-19 16:12:42 +0000

    more user stuff + crypto

--- src/buddylist/users.clj
+++ src/buddylist/users.clj
@@ -1,9 +1,19 @@
 (ns buddylist.users
     (:require [duratom.core :as duratom]))
 
+(import java.security.MessageDigest)
+
 (def data (duratom :local-file
          :file-path "/home/ubuntu/BuddyList/data.duratom"
-         :init []))
+         :init {}))
+
+(defn sha256 [string]
+  (let [digest (.digest (MessageDigest/getInstance "SHA-256") (.getBytes string "UTF-8"))]
+    (apply str (map (partial format "%02x") digest))))
 
 (defn create-user! [username cleartext-password phone]
-    ())
+    (assoc username {
+        :username username
+        :password-hash (sha256 cleartext-password)
+        :phone phone
+    }))
