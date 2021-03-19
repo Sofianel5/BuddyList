1e2d5cdb97fc17cf3c91025907cec8bb271b1aab
Ubuntu <ubuntu@ip-172-31-78-140.ec2.internal>
2021-03-19 19:23:28 +0000

    got local db to work

--- src/buddylist/users.clj
+++ src/buddylist/users.clj
@@ -3,8 +3,8 @@
   (:import [java.security MessageDigest]))
 
 (def data (duratom/duratom :local-file
-                   :file-path "/home/ubuntu/BuddyList/data.duratom"
-                   :init {}))
+                           :file-path "/home/ubuntu/BuddyList/data.duratom"
+                           :init {}))
 (defn sha256 [string]
   (let [digest (.digest (MessageDigest/getInstance "SHA-256") (.getBytes string "UTF-8"))]
     (apply str (map (partial format "%02x") digest))))
@@ -12,16 +12,17 @@
 (defn gen-auth-token []
   (sha256 (str 1)))
 
-
 (defn create-user! [username cleartext-password phone]
-  (get (assoc data username {:username username
-                             :password-hash (sha256 cleartext-password)
-                             :phone phone
-                             :buddies []
-                             :auth-token ""}) username))
-(comment 
-  (create-user! "sofiane" "password" "9179570254")
-)
+  (swap! data assoc username {:username username
+                                   :password-hash (sha256 cleartext-password)
+                                   :phone phone
+                                   :buddies []
+                                   :auth-token ""}))
+(comment
+  (create-user! "sofiane" "password" "9179570254"))
+  (comment
+    @data
+  )
 
 (defn delete-user! [username])
 
