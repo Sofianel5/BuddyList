9806f13aab707357f9a9f1c2d228e7212576f3d8
Ubuntu <ubuntu@ip-172-31-78-140.ec2.internal>
2021-03-19 15:50:03 +0000

    simple webserver

--- src/buddylist/core.clj
+++ src/buddylist/core.clj
@@ -1,10 +1,18 @@
 (ns buddylist.core
     (:require [org.httpkit.server :as http-kit]
-              [duratom.core :as duratom]))
+              [duratom.core :as duratom]
+              [compojure.route :only [files not-found]]
+              [compojure.core :only [defroutes GET POST DELETE ANY context]]))
 
 (duratom :local-file
          :file-path "/home/ubuntu/BuddyList/data.duratom"
          :init {:x 1 :y 2})
+
+(defn render [req]
+  (println (pr-str req))
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
 
 (defn handler [request]
   (with-channel request channel
@@ -12,7 +20,13 @@
     (on-receive channel (fn [data] ;; echo it back
                           (send! channel data)))))
 
+(defroutes all-routes
+  (GET "/" [] render)
+  (GET "/ws" [] handler)     ;; websocket
+  (files "/static/") ;; static file url prefix /static, in `public` folder
+  (not-found "<p>Page not found.</p>")) ;; all other, return 404
+
 (defn -main [& args]
   (let [p 8000]
-    (http-kit/run-server handler {:ip "0.0.0.0", :port p})
+    (http-kit/run-server all-routes {:ip "0.0.0.0", :port p})
     (println "Server running on port" p)))
